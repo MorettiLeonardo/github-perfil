dc40a3acb8755aceba3edce6753d878e4a0cfc18
Leonardo Moretti <leonardonabarro2003@gmail.com>
2023-11-21 15:26:05 -0300

    arrumando o input para pesquisar users

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import { useState } from "react"
 import Perfil from "./components/Perfil"
 import ReposList from "./components/ReposList"
 
-function App() {  
+function App() {
   const [username, setUsername] = useState('')
 
   return (
     <>
-      <input type="text" onBlur={e => setUsername(e.target.value)} />
-      {username.length > 4 && (
-        <div>
+      {username.length > 4 ?
+        <div >
           <Perfil nomeDoUsuario={username} />
-
           <ReposList nomeDoUsuario={username} />
+        </div > :
+        
+        <div className="pesquisa">
+          <h1>Digite o nome de usuario do github</h1>
+          <input type="text" onBlur={e => setUsername(e.target.value)} />
         </div>
-      )}
+      }
     </>
   )
 }
